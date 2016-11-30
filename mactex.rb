afeef84985e95b11a4d8b0f89e8244ca06396469
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-30 19:31:49 +0800

    Update mactex.rb

--- mactex.rb
+++ mactex.rb
@@ -1,6 +1,6 @@
 cask 'mactex' do
   version '20161009'
-  sha256 'b44873d445881900401d0e0eddccc78140b9ed51b538364889eb8529350d5bd7'
+  sha256 :no_check
 
   url "http://dante.ctan.org/tex-archive/systems/mac/mactex/mactex-#{version}.pkg"
   name 'MacTeX'
