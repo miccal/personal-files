7107741515dac73f547017e20f4848aeb4da35cd
miccal <miccal.matthews@gmail.com>
2018-01-04 12:27:43 +0800

    Update mactex.rb

--- mactex.rb
+++ mactex.rb
@@ -2,7 +2,7 @@ cask 'mactex' do
   version '20170524'
   sha256 :no_check
 
-  url "http://dante.ctan.org/tex-archive/systems/mac/mactex/mactex-#{version}.pkg"
+  url "http://mirror.aarnet.edu.au/pub/CTAN/systems/mac/mactex/mactex-#{version}.pkg"
   name 'MacTeX'
   homepage 'https://www.tug.org/mactex/'
 
