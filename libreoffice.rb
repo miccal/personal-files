a1d3e818335331e91e8d0efa919a022b40ca5238
miccal <miccal.matthews@gmail.com>
2018-01-04 12:28:43 +0800

    Update libreoffice.rb

--- libreoffice.rb
+++ libreoffice.rb
@@ -3,18 +3,8 @@ cask 'libreoffice' do
   sha256 '4f081ae90e96c0f3c5573705fcdd763027ceae16f83ec47e6afb40a735a44ce3'
 
   url "http://mirror.internode.on.net/pub/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
-  appcast 'https://download.documentfoundation.org/libreoffice/stable/',
-          checkpoint: 'e10b6d6d5b9d7d21174049291ce78cb5eca63d91ccf5aaed3f0b6715022629ca'
   name 'LibreOffice'
   homepage 'https://www.libreoffice.org/'
-  gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
-
-  auto_updates true
-  conflicts_with cask: [
-                         'libreoffice-rc',
-                         'libreoffice-still',
-                       ]
-  depends_on macos: '>= :mountain_lion'
 
   app 'LibreOffice.app'
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/gengal"
@@ -37,11 +27,4 @@ cask 'libreoffice' do
       '#{appdir}/LibreOffice.app/Contents/MacOS/soffice' "$@"
     EOS
   end
-
-  zap trash: [
-               '~/Library/Application Support/LibreOffice',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.libreoffice.script.sfl*',
-               '~/Library/Preferences/org.libreoffice.script.plist',
-               '~/Library/Saved Application State/org.libreoffice.script.savedState',
-             ]
 end
