b5e454b43ea4a74b4efdf1c9c715c5f141b10fa1
miccal <miccal.matthews@gmail.com>
2020-04-08 05:22:54 +0800

    Update inna-beta.rb

--- inna-beta.rb
+++ inna-beta.rb
@@ -1,6 +1,6 @@
 cask 'iina-beta' do
   version '1.0.7-beta1'
-  sha256 '40fb3442705e8ca1ab27c16e6dbdab9d41f926e4eeecb65c8f7829196b9cb863'
+  sha256 :no_check
 
   url "https://dl.iina.io/IINA.v#{version}.dmg"
   name 'IINA'
